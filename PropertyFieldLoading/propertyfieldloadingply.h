44a15f951c2959078516086b60252b5ec3f2e291
Gonzalo Infante <ginfante@dcc.uchile.cl>
2016-01-13 13:52:11 -0300

    Fixed and enhanced the code to load property fields from ply format.

--- PropertyFieldLoading/propertyfieldloadingply.h
+++ PropertyFieldLoading/propertyfieldloadingply.h
@@ -16,7 +16,6 @@ class VertexCloud;
 class PolygonMesh;
 class PolyhedronMesh;
 class Model;
-struct VScalarDef;
 
 class PropertyFieldLoadingPly: public PropertyFieldLoadingStrategy, public ModelVisitor
 {
@@ -32,46 +31,6 @@ class PropertyFieldLoadingPly: public PropertyFieldLoadingStrategy, public Model
 
 		std::string filename;
 		std::vector<std::shared_ptr<PropertyFieldDef>> selectedProperties;
-};
-
-class PropertyReader : public PropertyFieldDefVisitor {
-	std::ifstream* file;
-	vis::Vertex* vertex;
-	std::string line;
-	std::unique_ptr<std::istringstream> iss;
-	int currentIndex;
-	public:
-		PropertyReader(std::ifstream* file) : file(file){}
-
-		void setup(vis::Vertex* vertex, int index) {
-			FileUtils::safeGetline(*file, line);
-			iss.reset(new std::istringstream(line));
-			this->vertex = vertex;
-			currentIndex = index;
-		}
-
-		void skipProperty() {
-			float value = 0.0;
-			*iss >> value;
-		}
-
-		void visit(std::shared_ptr<ScalarFieldDef> scalarFieldDef)
-		{
-			//std::vector<float> properties;
-			//std::map<int, std::shared_ptr<ScalarFieldDef>> propertyIds;
-			//for(std::shared_ptr<PropertyFieldDef> prop : scalarProperties) {
-			//	propertyIds.insert(std::make_pair(prop->getId(), prop));
-			//}
-			float value = 0.0;
-
-			*iss >> value;
-			//std::vector<std::shared_ptr<PropertyFieldDef>>::iterator it = find(propertyIds.begin(), propertyIds.end(), i);
-			vertex->addProperty(currentIndex,value);
-			scalarFieldDef->expandBounds(value);
-			currentIndex++;
-			//propertyIds[i]->expandBounds(value);
-		}
-
 };
 
 #endif // PROPERTYFIELDLOADINGPLY_H

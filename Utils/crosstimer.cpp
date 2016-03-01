40135f4ab9ee2f1b4687f6662407821643a11b3a
Gonzalo Infante <chalo.infante01@gmail.com>
2016-03-01 03:06:36 -0300

    Enhanced the crosstimer class to be better compatible on Windows platforms.

--- Utils/crosstimer.cpp
+++ Utils/crosstimer.cpp
@@ -1,4 +1,37 @@
 #include "crosstimer.h"
+#include <iostream>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include <windows.h>
+
+CrossTimer::CrossTimer(){
+	if(!QueryPerformanceFrequency(&pcFreq))
+		std::cout << "QueryPerformanceFrequency failed!\n";
+
+	QueryPerformanceCounter(&startTime);
+}
+long CrossTimer::getTranscurredMilliseconds(){
+	LARGE_INTEGER result;
+	QueryPerformanceCounter(&result);
+	result.QuadPart-=startTime.QuadPart;
+	result.QuadPart *= 1000;
+	return long(result.QuadPart/pcFreq.QuadPart);
+}
+
+float CrossTimer::getTranscurredSeconds(){
+	LARGE_INTEGER result;
+	QueryPerformanceCounter(&result);
+	result.QuadPart-=startTime.QuadPart;
+	return float(result.QuadPart)/pcFreq.QuadPart;
+}
+long CrossTimer::getTranscurredNanoseconds(){
+	LARGE_INTEGER result;
+	QueryPerformanceCounter(&result);
+	result.QuadPart-=startTime.QuadPart;
+	result.QuadPart *= 1000000000;
+	return long(result.QuadPart/pcFreq.QuadPart);
+}
+#else
+
 
 CrossTimer::CrossTimer():createdAt(std::chrono::high_resolution_clock::now()){
 
@@ -17,3 +50,4 @@ long CrossTimer::getTranscurredNanoseconds(){
 	auto t2 = std::chrono::high_resolution_clock::now();
 	return std::chrono::duration_cast<std::chrono::nanoseconds>(t2-createdAt).count();
 }
+#endif
--- Utils/crosstimer.h
+++ Utils/crosstimer.h
@@ -1,6 +1,9 @@
 #ifndef CROSSTIMER_H
 #define CROSSTIMER_H
 #include <chrono>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include <windows.h>
+#endif
 //#include <ctime>
 class CrossTimer
 {
@@ -10,8 +13,13 @@ class CrossTimer
 		float getTranscurredSeconds();
 		long getTranscurredNanoseconds();
 	private:
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+		LARGE_INTEGER pcFreq;
+		LARGE_INTEGER startTime;
+#else
 		std::chrono::time_point<std::chrono::system_clock> createdAt;
 		//int t;
+#endif
 };
 
 #endif // CROSSTIMER_H
